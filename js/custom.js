9af2f115524fab928b532214d663aa1b9e548e9b
Josh Harrison <jarshvader@gmail.com>
2020-11-03 12:51:19 -0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+	$(".js-height-full").height($(window).height());
+	$(".js-height-parent").each(function() {
+	    $(this).height($(this).parent().first().height());
+	});
+
+	// Fun Facts
+	function count($this) {
+	    var current = parseInt($this.html(), 10);
+	    current = current + 1; /* Where 50 is increment */
+
+	    $this.html(++current);
+	    if (current > $this.data('count')) {
+	        $this.html($this.data('count'));
+	    } else {
+	        setTimeout(function() {
+	            count($this)
+	        }, 50);
+	    }
+	}
+
+	$(".stat-timer").each(function() {
+	    $(this).data('count', parseInt($(this).html(), 10));
+	    $(this).html('0');
+	    count($(this));
+	});
+
+	$('.header').affix({
+	    offset: {
+	        top: 100,
+	        bottom: function() {
+	            return (this.bottom = $('.footer').outerHeight(true))
+	        }
+	    }
+	})
+
+	$(window).load(function() {
+	    $("#preloader").on(001).fadeOut();
+	    $(".preloader").on(600).fadeOut("slow");
+	});
+
+	if (navigator.userAgent.match(/SAMSUNG|Samsung|SGH-[I|N|T]|GT-[I|N]|SM-[A|N|P|T|Z]|SHV-E|SCH-[I|J|R|S]|SPH-L/i)) {
+
+	    document.getElementById('about').style.display='none';
+	    // your code for Samsung Smartphones goes here...
+	}
---
+++ js/textCrawl.js
@@ -0,0 +1,20 @@
+﻿var nbrStars = 1000;
+
+for (let i = 0; i < nbrStars; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+    var xy = rndPos();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    document.body.append(star);
+}
+
+function rndPos() {
+    var x = window.innerHeight;
+    var y = window.innerWidth;
+    var rndX = Math.floor(Math.random() * x);
+    var rndY = Math.floor(Math.random() * y);
+    return [rndX, rndY];
+}
+
+
